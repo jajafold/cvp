4c7f615beab036470cd127ba169e9aea4c1a8ec5
Dyrda Lev <akronman0000@gmail.com>
2022-11-06 14:58:41 +0500

    Re-branch

---
+++ Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿
+namespace VideoPlayer2
+{
+    partial class Player
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Player
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Name = "Player";
+            this.Load += new System.EventHandler(this.Player_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.AudioVideoPlayback;
+
+namespace VideoPlayer2
+{
+    public partial class Player : Form
+    {
+        public Player()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form1 = new Form();
+            form1.Show();
+            form1.WindowState = FormWindowState.Maximized;
+            Video movie = new Video("C:\\Users\\Оля\\Desktop\\фото\\дпмм\\IMG_2162.MOV");
+            movie.Owner = form1;
+            movie.Size = new Size(1920, 1080);
+            movie.Play();
+        }
+
+        private void Player_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+    }
+}
+
